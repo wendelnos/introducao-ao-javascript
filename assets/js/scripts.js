2eb67c3953cc3a4e11611640632d6649ef71702d
wendelnos <wendelnos@gmail.com>
2022-07-14 23:50:59 -0300

    Desafio do Curso introducao ao JavaScript

---
+++ assets/js/scripts.js
@@ -0,0 +1,51 @@
+
+/*
+var currentNumberWrapper = document.getElementById('currentNumber');
+var currentNumber = 0;
+
+function decrement(){
+    
+    currentNumber = currentNumber -1; 
+    currentNumberWrapper.innerHTML = currentNumber;
+}
+
+function increment(){
+    currentNumber = currentNumber +1; 
+    currentNumberWrapper.innerHTML = currentNumber;
+}
+*/
+
+
+var currentNumberWrapper = document.getElementById('currentNumber');
+const elementSub = document.getElementById("subtrair");
+const elementSoma = document.getElementById("adicionar");
+var currentNumber = 0;
+
+elementSub.addEventListener("click", subtrair);
+elementSoma.addEventListener("click",somar);
+
+function subtrair() {
+    if (currentNumber>-10) {
+        currentNumber = currentNumber - 1;
+        atribui(currentNumber);
+    }
+    
+  }
+
+function somar() {
+    if (currentNumber<10) {
+     currentNumber = currentNumber + 1;
+     atribui(currentNumber);
+    }
+  }
+
+  function atribui(currentNumber){
+    document.getElementById("currentNumber").innerHTML = currentNumber;
+    if (currentNumber > 0) 
+    document.body.style.background = 'aqua';
+    else 
+    document.body.style.background = 'red';
+    
+  }
+
+  
